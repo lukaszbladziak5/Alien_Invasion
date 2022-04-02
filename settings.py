608519ca18af7c9e915d8f10985daff099df35ad
Lukasz B <lukasz.bladziak200005@gmail.com>
2022-04-02 14:42:00 +0200

    Dodanie modułu ustawień.

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+	"""Klasa przeznaczona do przechowywania wszystkich ustawień gry."""
+
+	def __init__(self):
+		"""Inicjalizacja gry."""
+
+		#Ustawienia ekranu.
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (230, 230, 230)
