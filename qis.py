731d94113f3481acbb33d8195b3f0a6ae3b058fb
Lukasz B <lukasz.bladziak200005@gmail.com>
2022-04-18 23:08:25 +0200

    Dodanie pocisków.

---
+++ qis.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+
+from qiskit import QuantumRegister, ClassicalRegister
+from qiskit import QuantumCircuit, execute,IBMQ
+from qiskit.tools.monitor import job_monitor
+
+IBMQ.enable_account('fd3b7dde427065d5045932ab6939a3eee6f42786a83e274d8b620032e3912a47040d6e30fd0d2ec7625c5561dbe14867aad5f9168f15a5f70b6e984cb75d35d4')
+provider = IBMQ.get_provider(hub='ibm-q')
+
+backend = provider.get_backend('ibmq_qasm_simulator')
+
+    
+q = QuantumRegister(8,'q')
+c = ClassicalRegister(8,'c')
+
+circuit = QuantumCircuit(q,c)
+
+
+circuit.ch(q[0], q[1]);
+circuit.x(q[2])
+circuit.ch(q[2], q[3]);
+circuit.x(q[4])
+circuit.ch(q[4], q[5]);
+circuit.x(q[6])
+circuit.ch(q[6], q[7]);
+circuit.measure(q,c)
+
+print(circuit)
+
+job = execute(circuit, backend, shots=20000)
+
+job_monitor(job)
+counts = job.result().get_counts()
+
+print(counts)
+plt.hist(counts, rwidth = 0.8)
+plt.show()
---
+++ zadania/gameSettings.py
@@ -0,0 +1,6 @@
+class GameSettings():
+	def __init__(self):
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (128, 128, 0)
+		self.rocket_speed = 1.5
